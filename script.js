2bf6dab10343a30b1431256890f7ead3e45e7b00
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-09-29 21:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function functionName() {
+    // This alerts the name of the current function
+    alert(arguments.callee.name);
+}
+
+// You can call it like this to test:
+functionName();
+AccioJob();   
